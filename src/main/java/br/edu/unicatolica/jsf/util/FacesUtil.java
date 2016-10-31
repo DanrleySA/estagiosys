00af1bafd5b91800d61b1d87e0d3509a7f0eaf9e
Danrley <Danrley@Danrley.local>
2016-10-31 10:30:17 -0300

    Mensagens e validação da quantidade de venda.

--- src/main/java/br/edu/unicatolica/jsf/util/FacesUtil.java
+++ src/main/java/br/edu/unicatolica/jsf/util/FacesUtil.java
@@ -5,14 +5,14 @@ import javax.faces.context.FacesContext;
 
 public class FacesUtil {
 
-	public static void addErrorMessage(String message) {
-		FacesContext.getCurrentInstance().addMessage(null,
-				new FacesMessage(FacesMessage.SEVERITY_ERROR, message, message));
-	}
+    public static void addErrorMessage(String message) {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, message, message));
+    }
 
-	public static void addInfoMessage(String message) {
-		FacesContext.getCurrentInstance().addMessage(null,
-				new FacesMessage(FacesMessage.SEVERITY_INFO, message, message));
-	}
+    public static void addInfoMessage(String message) {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, message, message));
+    }
 
 }
